221abbce1153d5e7bc1d05c8936734adaaa5262e
KNicole <38897127+Katynicole89@users.noreply.github.com>
2021-10-04 18:02:13 -0500

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 
 router.use(require('./candidatesRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
+
+
 
 module.exports = router; 
